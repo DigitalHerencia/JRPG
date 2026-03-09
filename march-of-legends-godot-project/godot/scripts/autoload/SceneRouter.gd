8d590a6116920f0d9ca7d7932e4c7f43ba112bf8
Ivan Roman <DigitalHerencia@Outlook.com>
2026-03-09 14:36:53 -0600

    Add GameState mutation signals and save/load APIs

--- march-of-legends-godot-project/godot/scripts/autoload/SceneRouter.gd
+++ march-of-legends-godot-project/godot/scripts/autoload/SceneRouter.gd
@@ -11,5 +11,5 @@ func goto(route_key: String) -> void:
 	if not routes.has(route_key):
 		push_error("Unknown route: %s" % route_key)
 		return
-	GameState.current_scene_key = route_key
+	GameState.set_current_scene_by_key(route_key, routes[route_key])
 	get_tree().change_scene_to_file(routes[route_key])
